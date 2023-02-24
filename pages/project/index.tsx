5e2e54e81c05e1bc715056381d77a197596fb53e
earthflex <earthflexible@gmail.com>
2023-02-25 02:33:15 +0700

    fix

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -7,14 +7,14 @@ export default function Project() {
             <img className="mbm-lighten pe-none" src="/error.png" width={200} alt={"error page"} />
             <div className="d-flex error-text">
                 <motion.h1
-                    initial={{ x: 100, opacity: 0 }}
-                    animate={{ x: 0, opacity: 1 }}
+                    initial={{ y: 100, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
                 >
                     🔔
                 </motion.h1>
                 <motion.h2
-                    initial={{ x: -200, opacity: 0 }}
-                    animate={{ x: 0, opacity: 1 }}>
+                    initial={{ y: -100, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}>
                     Portfolio is currently being updated.
                 </motion.h2>
             </div>
