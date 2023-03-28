2d39ee864c275807b0a818e2666afac98ab98811
earthflex <earthflexible@gmail.com>
2023-03-28 13:12:03 +0700

    fix : domain

--- component/layout/meta.tsx
+++ component/layout/meta.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-const DOMAIN = "https://2275-49-49-233-191.ap.ngrok.io";
+const DOMAIN = "https://earthflex.dev";
 
 export default function Meta({
     title = "Earthflex.dev",
