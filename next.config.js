bb748c90610092d8e7c8552de337d5df3f46d137
earthflex <earthflexible@gmail.com>
2023-05-05 00:39:48 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    swcMinify: true,
-    loader: 'custom',
-    formats: ['image/avif', 'image/webp'],
-  },
+  swcMinify: true,
+  // images: {
+  //   loader: 'custom',
+  //   formats: ['image/avif', 'image/webp'],
+  // },
 }
 
 module.exports = nextConfig
