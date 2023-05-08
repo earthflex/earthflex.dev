79758f6a81a8dbdd20b8d2aa615659a50a7f10e2
earthflex <earthflexible@gmail.com>
2023-05-08 15:08:26 +0700

    fix

--- lib/motions.ts
+++ lib/motions.ts
@@ -1,6 +1,11 @@
 
 import { Variants } from "framer-motion";
 
+export const headerFixed: Variants = {
+    hidden: { y: -100, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+}
+
 export const staggerContainer: Variants = {
     initial: {},
     animate: {
@@ -11,18 +16,32 @@ export const staggerContainer: Variants = {
     }
 }
 
-// export const textAnimate: Variants = {
-//     offscreen: { y: 100, opacity: 0 },
-//     onscreen: {
-//         y: 0,
-//         opacity: 1,
-//         transition: {
-//             type: "spring",
-//             bounce: 0.4,
-//             duration: 1
-//         }
-//     }
-// }
+export const imageAnimate: Variants = {
+    offscreen: { x: -100, opacity: 0 },
+    onscreen: {
+        x: 0,
+        opacity: 1,
+        rotate: [0, 10, 0],
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 1
+        }
+    }
+}
+
+export const textAnimate: Variants = {
+    offscreen: { y: 100, opacity: 0 },
+    onscreen: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 1
+        }
+    }
+}
 
 export const earthflexText: Variants = {
     initial: {
