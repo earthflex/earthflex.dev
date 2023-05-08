97aebbbf54c29e9be5934969300126fcf15c6ac3
earthflex <earthflexible@gmail.com>
2023-05-08 22:57:11 +0700

    fix

---
+++ components/shared/spotify-embed.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface SpotifyEmbedProps {
+    type: 'track' | 'album' | 'playlist';
+    id: string;
+    width?: string;
+    height?: string;
+}
+
+export const SpotifyEmbed: React.FC<SpotifyEmbedProps> = ({
+    type,
+    id,
+    width = '100%',
+    height = '152',
+}) => {
+
+    const embedUrl = `https://open.spotify.com/embed/${type}/${id}?utm_source=generator&theme=0`;
+
+    return (
+        <iframe style={{ borderRadius: '14px' }}
+            src={embedUrl}
+            title="Spotify Embed"
+            width={width}
+            height={height}
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy" />
+    );
+};
