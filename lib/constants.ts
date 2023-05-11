d987932c0c6ac86128a64cd56e58f392e44190e9
earthflex <earthflexible@gmail.com>
2023-05-12 02:26:47 +0700

    fix

--- lib/constants.ts
+++ lib/constants.ts
@@ -22,10 +22,42 @@ export const ButtonScale = (direction: "up" | "down" = "up"): Variants => {
 };
 
 export const headerHover: Variants = {
-  initial: { scale: 1},
+  initial: { scale: 1 },
   whileHover: { scale: .99 },
   whileTap: { scale: 1 },
 };
+export const cardTap: Variants = {
+  initial: {
+   background:'linear-gradient(136deg, rgb(51, 51, 51) 0%, rgb(34, 34, 34) 100%)',
+    transition: {
+      duration: 0.5
+    }
+  },
+  whileHover: {
+    scale: .99,
+    background: [
+      'linear-gradient(136deg, rgb(51, 51, 51) 0%, rgb(84, 84, 84) 100%)',
+      'linear-gradient(136deg, rgb(84, 84, 84) 0%, rgb(51, 51, 51) 100%)',
+    ],
+    transition: {
+      duration: 1.2,
+      ease: [0, 0.71, 0.2, 1.01],
+      background: {
+        type: "spring",
+        damping: 20,
+        stiffness: 80,
+        restDelta: 0.001,
+        repeat: Infinity,
+      }
+    }
+  },
+  whileTap: {
+    scale: 1.01,
+    transition: {
+      duration: 0.5
+    }
+  },
+}
 
 export const sentence: Variants = {
   hidden: { opacity: 1, scale: .5 },
