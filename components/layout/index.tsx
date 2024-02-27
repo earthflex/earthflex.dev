bbdd4f5208421c5cef6c7c9edbf9c9bbd41ac635
earthflex <earthflexible@gmail.com>
2024-02-27 18:26:15 +0700

    hotjar : setup

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -8,75 +8,81 @@ import useModal from "../hook/use-modal";
 import ResumeDetail from "../resume";
 import Modal from "../shared/modal/modal";
 
-import { MetaProps, EXPERIENCE_TYPE, PROFILE_TYPE, WORKS_ITEMS_TYPE } from "@/types";
+import {
+  MetaProps,
+  EXPERIENCE_TYPE,
+  PROFILE_TYPE,
+  WORKS_ITEMS_TYPE,
+} from "@/types";
 import { DataContext } from "@/context";
 import { AnimatePresence, motion } from "framer-motion";
 import useWindowSize from "../hook/use-window-size";
 import Cookies from "../shared/cookies";
 
+import Hotjar from "@hotjar/browser";
+const siteId = 3882722;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
 export default function Layout({
-    meta,
-    children,
-    works,
-    profile,
-    experience,
+  meta,
+  children,
+  works,
+  profile,
+  experience,
 }: {
-    meta?: MetaProps;
-    children: ReactNode;
-    works: WORKS_ITEMS_TYPE;
-    profile: PROFILE_TYPE;
-    experience: EXPERIENCE_TYPE;
+  meta?: MetaProps;
+  children: ReactNode;
+  works: WORKS_ITEMS_TYPE;
+  profile: PROFILE_TYPE;
+  experience: EXPERIENCE_TYPE;
 }) {
+  const { isDesktop, isTablet } = useWindowSize();
+  const { modalOpen, close, open } = useModal();
 
-    const { isDesktop, isTablet } = useWindowSize();
-    const { modalOpen, close, open } = useModal();
+  const scaleValue = isDesktop ? 0.95 : isTablet ? 0.95 : 0.9;
 
-    const scaleValue = isDesktop ? 0.95 : (isTablet ? 0.95 : 0.9);
+  const variants = {
+    open: {
+      y: 60,
+      scale: scaleValue,
+      borderRadius: "30px",
+      border: "2px solid rgba(221, 221, 221, 0.2)",
+      transition: { duration: 0.3, ease: [0.43, 0.13, 0.23, 0.96] },
+    },
+    closed: {
+      y: 0,
+      scale: 1,
+      borderRadius: "30px 30px 0 0",
+      border: "0 solid rgba(221, 221, 221, 0.2)",
+      transition: { duration: 0.3 },
+    },
+  };
 
-    const variants = {
-        open: {
-            y: 60,
-            scale: scaleValue,
-            borderRadius: "30px",
-            border: "2px solid rgba(221, 221, 221, 0.2)",
-            transition: { duration: 0.3, ease: [0.43, 0.13, 0.23, 0.96] }
-        },
-        closed: {
-            y: 0,
-            scale: 1,
-            borderRadius: "30px 30px 0 0",
-            border: '0 solid rgba(221, 221, 221, 0.2)',
-            transition: { duration: 0.3 }
-        }
-    };
-
-    return (
-        <React.Fragment>
-            <DataContext.Provider value={{ works, profile, experience }}>
-                <Meta {...meta} />
-                <Topbar onClick={open} />
-                <motion.div
-                    className="wrapper-main"
-                    variants={variants}
-                    animate={modalOpen ? "open" : "closed"}
-                >
-                    <Header />
-                    <main>
-                        {children}
-                    </main>
-                </motion.div>
-                <Dock onClick={open} />
-                <AnimatePresence>
-                    {modalOpen && (
-                        <Modal modalOpen={modalOpen} handleClose={close} >
-                            <ResumeDetail handleClose={close} />
-                        </Modal>
-                    )}
-                </AnimatePresence>
-                <Cookies />
-            </DataContext.Provider>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <DataContext.Provider value={{ works, profile, experience }}>
+        <Meta {...meta} />
+        <Topbar onClick={open} />
+        <motion.div
+          className="wrapper-main"
+          variants={variants}
+          animate={modalOpen ? "open" : "closed"}
+        >
+          <Header />
+          <main>{children}</main>
+        </motion.div>
+        <Dock onClick={open} />
+        <AnimatePresence>
+          {modalOpen && (
+            <Modal modalOpen={modalOpen} handleClose={close}>
+              <ResumeDetail handleClose={close} />
+            </Modal>
+          )}
+        </AnimatePresence>
+        <Cookies />
+      </DataContext.Provider>
+    </React.Fragment>
+  );
 }
-
-
