de20c8fa9cebd2c48b041f40261c2332643c047f
earthflex <earthflexible@gmail.com>
2024-02-27 18:31:07 +0700

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
 import { Provider as RWBProvider } from "react-wrap-balancer";
 import { Analytics } from "@vercel/analytics/react";
-import '@/styles/globals.css'
-import '@/styles/main.css'
-import '@/styles/responsive.css'
-import localFont from '@next/font/local'
+import "@/styles/globals.css";
+import "@/styles/main.css";
+import "@/styles/responsive.css";
+import localFont from "@next/font/local";
 import { motion, AnimatePresence } from "framer-motion";
-import SmoothScroll from '@/components/shared/smooth-scroll';
+import SmoothScroll from "@/components/shared/smooth-scroll";
 
 const clashdisplay = localFont({
   src: [
     {
-      path: '../styles/fonts/clashdisplay/fonts/ClashDisplay-Variable.woff2',
-      weight: '200 700',
-      style: 'normal',
+      path: "../styles/fonts/clashdisplay/fonts/ClashDisplay-Variable.woff2",
+      weight: "200 700",
+      style: "normal",
     },
   ],
-})
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RWBProvider>
-      <div className={clashdisplay.className} >
+      <div className={clashdisplay.className}>
         <div className="grain-animation"></div>
-        <AnimatePresence >
+        <AnimatePresence>
           <motion.div>
             <Component {...pageProps} />
             <SmoothScroll />
@@ -33,5 +33,5 @@ export default function App({ Component, pageProps }: AppProps) {
       </div>
       <Analytics />
     </RWBProvider>
-  )
+  );
 }
